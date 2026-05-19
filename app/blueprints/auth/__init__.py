d8ed7324ff086c395ed748924f6e5f244c985d3e
= <=>
2026-05-19 16:17:47 +0200

    Auth Blueprints Phase 2 file creation (login,register and base)

--- app/blueprints/auth/__init__.py
+++ app/blueprints/auth/__init__.py
@@ -2,4 +2,5 @@ from flask import Blueprint
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-from . import routes, forms
+# Import routes here to avoid circular imports
+from . import routes
